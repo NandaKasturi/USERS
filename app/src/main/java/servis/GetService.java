e59e328393f6acef09fe8111391c1d58a98ba53a
NandaKasturi <nandakasturi@gmail.com>
2021-04-22 01:56:19 +0700

    first commit

---
+++ app/src/main/java/servis/GetService.java
@@ -0,0 +1,14 @@
+package servis;
+
+
+import java.util.List;
+
+import model.User;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetService {
+    @GET("/users")
+    Call<List<User>> getUserList();
+
+}
